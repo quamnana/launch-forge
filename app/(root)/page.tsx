37d97a139d9099e508813157b4a197a90a59c1bc
Nana Quam <quamgyambrah@gmail.com>
2025-02-28 14:08:48 -0500

    added custom tw utilities and shadcn ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-2xl">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">Home</h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Your Ideas and Vote on Your Ideas
+        </p>
+      </section>
     </>
   );
 }
