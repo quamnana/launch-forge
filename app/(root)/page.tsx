ef41872fa63c6b93b1efe7b73717b284366892a4
Nana Quam <quamgyambrah@gmail.com>
2025-02-28 15:12:31 -0500

    implemented search form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,23 @@
-export default function Home() {
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const { query } = await searchParams;
+
   return (
     <>
       <section className="pink_container">
-        <h1 className="heading">Home</h1>
+        <h1 className="heading">
+          Where Ideas Ignite, <br /> and Startups Take Flight.
+        </h1>
         <p className="sub-heading !max-w-3xl">
-          Submit Your Ideas and Vote on Your Ideas
+          Your Startup, Our Ecosystem – Let’s Build the Next Big Thing.
         </p>
+
+        <SearchForm query={query} />
       </section>
     </>
   );
